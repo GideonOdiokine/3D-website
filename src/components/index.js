496adea944d3289bfd91ff124da2c201610da981
Gideon Odiokine <gidysmartodiokineh@gmail.com>
2022-10-28 05:22:07 +0100

    remove unwanted files

--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import CustomButton from "./CustomButton";
-import ColorPicker from "./ColorPicker";
-import FilePicker from "./FilePicker";
-import AIPicker from "./AIPicker";
-import Tab from "./Tab";
-
-export { CustomButton, ColorPicker, FilePicker, AIPicker, Tab };
